dcb4a8ddb80cc8ec06405222eb5083e8544afbc1
D00288733 <D00288733@student.dkit.ie>
2026-05-06 14:52:08 +0100

    update tests

--- tests/test_interest.py
+++ tests/test_interest.py
@@ -5,7 +5,7 @@ def test_check_string():
     with pytest.raises(ValueError):
         calculate_interest("231213")
 
-def test_int_float():
+def tst_int_float():
     with pytest.raises(ValueError):
         calculate_interest(number)
 
@@ -17,4 +17,39 @@ def test_value_0():
     with pytest.raises(ValueError):
         calculate_interest(-23)
     
-def test_
+
+def test_deposit_tier_1(monkeypatch):
+    inputs = iter([800,0.03,24])
+    monkeypatch.setattr('builtins.input',lambda _: next(inputs))
+    outputs=[]
+    monkeypatch.setattr('builtins.print', lambda x: outputs.append(x))
+
+    calculate_interest(800)
+    assert outputs == ["Tier 1: Deposit: 800, Amount in Tier 1: 800, Interest from Tier 1: 24.00"]
+
+def test_deposit_tier_2(monkeypatch):
+    inputs = iter([5000,0.035,175])
+    monkeypatch.setattr('builtins.input',lambda _: next(inputs))
+    outputs=[]
+    monkeypatch.setattr('builtins.print', lambda x: outputs.append(x))
+
+    calculate_interest(5000)
+    assert outputs == ['Tier 1: Deposit: 5000, Amount in Tier 1: 1000, Interest from Tier 1: ''30.00',"Tier 2: Deposit: 5000, Amount in Tier 2: 4000, Interest from Tier 2: 140.00"]
+
+def test_deposit_tier_3(monkeypatch):
+    inputs = iter([5000,0.035,175])
+    monkeypatch.setattr('builtins.input',lambda _: next(inputs))
+    outputs=[]
+    monkeypatch.setattr('builtins.print', lambda x: outputs.append(x))
+
+    calculate_interest(5000)
+    assert outputs == ['Tier 1: Deposit: 5000, Amount in Tier 1: 1000, Interest from Tier 1: ''30.00',"Tier 2: Deposit: 5000, Amount in Tier 2: 4000, Interest from Tier 2: 140.00"]
+
+def test_deposit_tier_4(monkeypatch):
+    inputs = iter([5000,0.035,175])
+    monkeypatch.setattr('builtins.input',lambda _: next(inputs))
+    outputs=[]
+    monkeypatch.setattr('builtins.print', lambda x: outputs.append(x))
+
+    calculate_interest(5000)
+    assert outputs == ['Tier 1: Deposit: 5000, Amount in Tier 1: 1000, Interest from Tier 1: ''30.00',"Tier 2: Deposit: 5000, Amount in Tier 2: 4000, Interest from Tier 2: 140.00"]
