711ad3d71180f92563b1de5ee27eb650144ea6a7
D00288733 <D00288733@student.dkit.ie>
2026-05-06 15:49:39 +0100

    code

--- tests/test_interest.py
+++ tests/test_interest.py
@@ -37,19 +37,19 @@ def test_deposit_tier_2(monkeypatch):
     assert outputs == ['Tier 1: Deposit: 5000, Amount in Tier 1: 1000, Interest from Tier 1: ''30.00',"Tier 2: Deposit: 5000, Amount in Tier 2: 4000, Interest from Tier 2: 140.00"]
 
 def test_deposit_tier_3(monkeypatch):
-    inputs = iter([5000,0.035,175])
+    inputs = iter([50000,0.04])
     monkeypatch.setattr('builtins.input',lambda _: next(inputs))
     outputs=[]
     monkeypatch.setattr('builtins.print', lambda x: outputs.append(x))
 
-    calculate_interest(5000)
-    assert outputs == ['Tier 1: Deposit: 5000, Amount in Tier 1: 1000, Interest from Tier 1: ''30.00',"Tier 2: Deposit: 5000, Amount in Tier 2: 4000, Interest from Tier 2: 140.00"]
+    calculate_interest(50000)
+    assert outputs == ['Tier 1: Deposit: 50000, Amount in Tier 1: 1000, Interest from Tier 1: ''30.00',"Tier 2: Deposit: 50000, Amount in Tier 2: 10000, Interest from Tier 2: 350.00", 'Tier 3: Deposit: 50000, Amount in Tier 3: 50000, Interest from Tier 3: 1560.00']
 
 def test_deposit_tier_4(monkeypatch):
-    inputs = iter([5000,0.035,175])
+    inputs = iter([101000,0.045,4545])
     monkeypatch.setattr('builtins.input',lambda _: next(inputs))
     outputs=[]
     monkeypatch.setattr('builtins.print', lambda x: outputs.append(x))
 
-    calculate_interest(5000)
-    assert outputs == ['Tier 1: Deposit: 5000, Amount in Tier 1: 1000, Interest from Tier 1: ''30.00',"Tier 2: Deposit: 5000, Amount in Tier 2: 4000, Interest from Tier 2: 140.00"]
+    calculate_interest(101000)
+    assert outputs == ['Tier 1: Deposit: 101000, Amount in Tier 1: 1000, Interest from Tier 1: ''30.00',"Tier 2: Deposit: 101000, Amount in Tier 2: 50000, Interest from Tier 2: 350.00", 'Tier 3: Deposit: 101000, Amount in Tier 3: 50000, Interest from Tier 3: 1560.00', ]
